4a42169a24e054371ecc38494642c484efba1e38
Ubuntu <ubuntu@ip-172-31-9-63.us-east-2.compute.internal>
2019-06-04 19:39:16 +0000

    create dockerfile

--- pipeline/codedeploy/build_image.sh
+++ pipeline/codedeploy/build_image.sh
@@ -4,5 +4,5 @@ echo "Building docker IMAGE "
 echo "Switching directory " `pwd` 
 export IMAGE_NAME=239144702963.dkr.ecr.us-east-2.amazonaws.com/octank-cicd
 export TAG=latest
-docker build -f dockerimage/Dockerfile -t $IMAGE_NAME:$TAG .
+docker build -f pipeline/dockerimage/Dockerfile -t $IMAGE_NAME:$TAG .
 docker tag $IMAGE_NAME:$TAG $IMAGE_NAME:$TAG
