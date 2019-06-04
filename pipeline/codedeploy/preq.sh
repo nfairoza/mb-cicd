6bb6787ac2df11f5303a0589207d286c272a013b
Ubuntu <ubuntu@ip-172-31-9-63.us-east-2.compute.internal>
2019-06-04 21:15:41 +0000

    edit pre build steps

--- pipeline/codedeploy/preq.sh
+++ pipeline/codedeploy/preq.sh
@@ -3,3 +3,8 @@ sudo su
 apt update
 wget -qO- https://get.docker.com/ | sh
 docker --version
+apt update
+apt install unzip
+apt install python-minimal -y
+pip install awscli --upgrade
+cp /usr/local/bin/aws /usr/bin/
