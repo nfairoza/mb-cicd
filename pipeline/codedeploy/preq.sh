2a8ca4ccd97347439759bac2b6eab7dc338be4d6
Ubuntu <ubuntu@ip-172-31-9-63.us-east-2.compute.internal>
2019-06-05 14:56:13 +0000

    preq.sh

--- pipeline/codedeploy/preq.sh
+++ pipeline/codedeploy/preq.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 apt-get update
-apt-get install \
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common \
-    unzip
+    unzip \
+    apt-utils \
+    python-minimal
 
-echo Installed apt preqs
+echo  ********************************* Installed apt preqs  *********************************
 curl -fsSL https://get.docker.com -o get-docker.sh
 bash get-docker.sh
 docker --version
-apt-get install docker docker-engine docker.io
-docker --version
-echo Installed Docker
+echo  ********************************* Installed Docker  *********************************
 
+# curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+# unzip awscli-bundle.zip
+# ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+# apt install awscli -y
 
-
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
-./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-
-apt install awscli -y
-
-aws --version
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+/usr/local/bin/pip install awscli --upgrade 
 
 cp /usr/local/bin/aws /usr/bin/
+
+aws --version
+aws ecr
+
+#cp /usr/local/bin/aws /usr/bin/
+
+echo  ********************************* End of preq.sh *********************************
