1e9f805fb077d653e58710a1c02aed1ed6b442d4
Ubuntu <ubuntu@ip-172-31-9-63.us-east-2.compute.internal>
2019-06-05 13:31:24 +0000

    edit preq.sh

--- pipeline/codedeploy/preq.sh
+++ pipeline/codedeploy/preq.sh
@@ -7,18 +7,13 @@ apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get install docker-ce
-docker --version
-apt update
-apt install unzip
-apt install python-minimal -y
+    software-properties-common \
+    docker-ce \
+    docker
+
+# apt update
+# apt install unzip
+# apt install python-minimal -y
 #pip install awscli --upgrade
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
