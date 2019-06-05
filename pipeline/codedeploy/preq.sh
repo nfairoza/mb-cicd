3d2c5fa9c2240a185e0c11ff6dd92ce4b4a53eca
Ubuntu <ubuntu@ip-172-31-9-63.us-east-2.compute.internal>
2019-06-05 13:36:48 +0000

    edit preq.sh

--- pipeline/codedeploy/preq.sh
+++ pipeline/codedeploy/preq.sh
@@ -12,12 +12,13 @@ apt-get install \
     docker
 
 # apt update
-# apt install unzip
-# apt install python-minimal -y
+apt install unzip
+apt install python-minimal -y
 #pip install awscli --upgrade
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 apt install awscli -y
+
 cp /usr/local/bin/aws /usr/bin/
