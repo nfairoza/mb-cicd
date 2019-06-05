bb55922d12ec6e57c162215b6b952228d15c1257
Ubuntu <ubuntu@ip-172-31-9-63.us-east-2.compute.internal>
2019-06-05 14:01:53 +0000

    edit preq.sh

--- pipeline/codedeploy/preq.sh
+++ pipeline/codedeploy/preq.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 #sudo su
 apt update
-apt-get remove docker docker-engine docker.io
 apt-get update
 apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common \
-    docker-ce \
-    docker
-
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 docker --version
+apt-get install docker docker-engine docker.io
+echo Installed Docker
 # apt update
 apt install unzip
 #apt install python-minimal -y
 
-#apt install python-pip -y
-apt install python3-pip -y
-pip3 install --upgrade pip
-
-pip3 install awscli --upgrade
+apt install python-pip -y
+#apt install python3-pip -y
+pip install --upgrade pip
+echo Successfully installed PIP
+pip install awscli --upgrade
 #pip install awscli --upgrade
 # curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 # unzip awscli-bundle.zip
