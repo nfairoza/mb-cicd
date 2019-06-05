4ce12cfec4133adca3a56c89259f6313e7aa3edd
Ubuntu <ubuntu@ip-172-31-9-63.us-east-2.compute.internal>
2019-06-05 15:18:48 +0000

    preq.sh

--- pipeline/codedeploy/build_image.sh
+++ pipeline/codedeploy/build_image.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-sudo su
 echo "Building docker IMAGE "
-#cd  pipeline/dockerimage
+#cd  ../dockerimage
 echo "Switching directory " `pwd` 
 export IMAGE_NAME=239144702963.dkr.ecr.us-east-2.amazonaws.com/octank-cicd
 export TAG=latest
-docker build -f pipeline/dockerimage/Dockerfile -t $IMAGE_NAME:$TAG .
+docker ps
+docker images
+docker build -f ../dockerimage/Dockerfile -t $IMAGE_NAME:$TAG .
 docker tag $IMAGE_NAME:$TAG $IMAGE_NAME:$TAG
--- pipeline/codedeploy/preq.sh
+++ pipeline/codedeploy/preq.sh
@@ -8,27 +8,14 @@ apt-get install -y \
     unzip \
     apt-utils \
     python-minimal
-
 echo  ********************************* Installed apt preqs  *********************************
 curl -fsSL https://get.docker.com -o get-docker.sh
 bash get-docker.sh
 docker --version
 echo  ********************************* Installed Docker  *********************************
-
-# curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-# unzip awscli-bundle.zip
-# ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-# apt install awscli -y
-
 curl -O https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 /usr/local/bin/pip install awscli --upgrade 
-
 cp /usr/local/bin/aws /usr/bin/
-
 aws --version
-aws ecr
-
-#cp /usr/local/bin/aws /usr/bin/
-
 echo  ********************************* End of preq.sh *********************************
