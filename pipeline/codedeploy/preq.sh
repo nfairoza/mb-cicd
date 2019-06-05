498cbaa122aaa67a173daf62c2a085f834aa4a17
Ubuntu <ubuntu@ip-172-31-9-63.us-east-2.compute.internal>
2019-06-05 13:43:43 +0000

    edit preq.sh

--- pipeline/codedeploy/preq.sh
+++ pipeline/codedeploy/preq.sh
@@ -13,12 +13,18 @@ apt-get install \
 
 # apt update
 apt install unzip
-apt install python-minimal -y
-#pip install awscli --upgrade
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
-./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+#apt install python-minimal -y
 
-apt install awscli -y
+#apt install python-pip -y
+apt install python3-pip
+pip3 install awscli --upgrade
+#pip install awscli --upgrade
+# curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+# unzip awscli-bundle.zip
+# ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+# apt install awscli -y
+
+aws --version
 
 cp /usr/local/bin/aws /usr/bin/
