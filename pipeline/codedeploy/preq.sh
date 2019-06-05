e3ea5069739c1f27f810885f7b02fa6a64fd6ff3
Ubuntu <ubuntu@ip-172-31-9-63.us-east-2.compute.internal>
2019-06-05 14:23:05 +0000

    preq.sh

--- pipeline/codedeploy/preq.sh
+++ pipeline/codedeploy/preq.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
-#sudo su
-apt update
 apt-get update
 apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common \
+    unzip
+
+echo Installed apt preqs
 curl -fsSL https://get.docker.com -o get-docker.sh
-get-docker.sh
+bash get-docker.sh
 docker --version
 apt-get install docker docker-engine docker.io
+docker --version
 echo Installed Docker
-# apt update
-apt install unzip
-apt install python-minimal -y
-apt install python-pip -y
-#apt install python3-pip -y
-pip install --upgrade pip
-echo Successfully installed PIP
-pip install awscli --upgrade
-#pip install awscli --upgrade
-# curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-# unzip awscli-bundle.zip
-# ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
-# apt install awscli -y
+
+
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+apt install awscli -y
 
 aws --version
 
